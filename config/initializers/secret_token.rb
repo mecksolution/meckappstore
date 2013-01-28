12a5c19b4bc0fca866b3a946ed1f33f495322105
Meck ERic <meckzed@gmail.com>
2013-01-28 19:42:37 +0300

    initial design

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meck::Application.config.secret_token = '6ee418284bf268e91d9460e7c6779280248e086ed8bfc9838a1322e1fcf4f390dcfa4e0e995c927947ac884b5e1221e9b31dfc8a3dbc80d84690046b0235247a'
